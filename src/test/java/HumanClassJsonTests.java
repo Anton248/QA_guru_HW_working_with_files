5e57f88ad8c0f3750c55172c6c52d440a65ac4a3
Anton Nikolski <ant.nikolski@gmail.com>
2023-05-01 23:30:50 +0300

    solved encoding problem

--- src/test/java/HumanClassJsonTests.java
+++ src/test/java/HumanClassJsonTests.java
@@ -9,7 +9,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 
-public class HumanClassTests {
+public class HumanClassJsonTests {
 
     @Test
     @DisplayName("Читаем объект Human is Json и проверяем")
@@ -22,7 +22,7 @@ public class HumanClassTests {
             Human human = mapper.readValue(isr, Human.class); //импортируем объект Human из файла human.json
 //Тест проходит, если только перекодировать литерал "Пётр" из UTF-8 в Windows-1251. В настройках IDEA везде где только
 //можно указал UTF-8. Литерал "Пётр" сравнивается с записью из файла human.json, который в UTF-8 (проверено).
-//И файл HumanClassTests.java, где литерал записан, также в кодировке UTF-8 (проверено). Можно ли как-то от перекодировки избавиться?
+//И файл HumanClassJsonTests.java, где литерал записан, также в кодировке UTF-8 (проверено). Можно ли как-то от перекодировки избавиться?
             String petr = new String ("Пётр".getBytes(StandardCharsets.UTF_8), "windows-1251"); //перекодируем литерал
             Assertions.assertEquals(petr, human.getName()); //сравниваем "Пётр" из строки выше с "Пётр" из файла human.json
             Assertions.assertEquals(40, human.getAge());
